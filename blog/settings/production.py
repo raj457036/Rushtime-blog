c893ae65c69100431c76db0ee61c37b8e0f77307
Raj singh <rajsingh.rs457036@gmail.com>
2018-07-16 15:22:38 +0530

    u

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATICFILES_DIRS = [os.path.join(os.path.dirname(os.path.abspath(__file__)),'static'),]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)),'staticserve')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticserve')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'user_login'
