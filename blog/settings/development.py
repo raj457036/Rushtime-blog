d985f188bbaad2aec88f28dfc8ee12da3a3cc7dd
Raj singh <rajsingh.rs457036@gmail.com>
2018-07-18 23:47:15 +0530

    d

--- blog/settings/development.py
+++ blog/settings/development.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECURITY_KEY'] if os.environ.get('SECURITY_KEY') != Non
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['rushtime.herokuapp.com','127.0.0.1']
 
 
 # Application definition
