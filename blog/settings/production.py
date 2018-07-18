14e7e999b5f5af45f617373785aa7c20a4f11334
Raj singh <rajsingh.rs457036@gmail.com>
2018-07-19 01:07:10 +0530

    security check

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -24,7 +24,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 SECRET_KEY = os.environ['SECURITY_KEY'] if os.environ.get('SECURITY_KEY') != None else '*j0(@)o5074i)8&s7vxv083b5m+-@+r)us&rrz87&tw5g0g5z@' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rushtime.herokuapp.com']
 
